19ca689337ff3db0cccfea87a2dd026450cffbc2
frea45 <110092585+frea45@users.noreply.github.com>
2023-04-25 12:48:08 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,7 @@ import os
 class Config:
     API_ID = int( os.getenv("api_id","3335796") )
     API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001621703668") )
+    CHANNEL = int( os.getenv("channel_files_chat_id","") )
     CHANNEL_USERNAME = os.getenv("channel_username","Seriesplus1")
-    TOKEN = "5535287993:AAFvr3OJAbuMwKEvBDgB0nwIKbmwoLNqw1A"
-    DOMAIN  = os.getenv("domain","https://neww-li-prod-divar-woaooq.mo5.mogenius.io")
+    TOKEN = ""
+    DOMAIN  = os.getenv("domain","")
